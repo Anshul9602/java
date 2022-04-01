c264bd6d4c07d5e44e177327dff33a671d397dd1
Anshul <81064196+Anshul9602@users.noreply.github.com>
2022-04-01 14:12:04 +0530

    literals in java

---
+++ literals.java
@@ -0,0 +1,19 @@
+public class literals {
+    public static void main(String[] args) {
+       // int age =20;
+        byte age =20;
+        char ch ='A';
+        long age2 = 577777777777l;//l sohowing the long number
+        short age3 =5;
+        float f1 = 5.6f;
+        double d1 = 4.55d;
+        boolean a = true;
+        System.out.println(age);
+        System.out.println(ch);
+        System.out.println(f1);
+        System.out.println(d1);
+        System.out.println(age3);
+        System.out.println(age2);
+        System.out.println(a);
+    }
+}
